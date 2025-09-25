c0b972fb65369e07ce87e6f8904c45dc79f03a2c
Mr Wiyar Ahmad Zai <mrwiyarahmadzai@gmail.com>
2025-09-25 12:13:22 +0430

    Fix base path for local development and production

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/Portfolio/',
+  base: command === 'build' ? '/Portfolio/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
@@ -14,4 +14,4 @@ export default defineConfig({
     host: true,
     port: 3000
   }
-});
+}));
