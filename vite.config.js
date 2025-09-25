5e01ccc8052131ee5aa64eae74657658415116ec
Mr Wiyar Ahmad Zai <mrwiyarahmadzai@gmail.com>
2025-09-25 11:07:36 +0430

    Update vite.config.js with build settings

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio/",
+  base: '/Portfolio/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  },
   server: {
     host: true,
     port: 3000
