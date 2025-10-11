e43e2b10b1731a0ee871921f4e023820288f2a46
Mr Wiyar Ahmad Zai <mrwiyarahmadzai@gmail.com>
2025-10-11 14:51:42 +0430

    Add DataProvider and Admin routes to App component for enhanced state management and admin access

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
+import { DataProvider } from './contexts/DataContext';
 import Navbar from './components/Navbar';
 import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
@@ -10,26 +11,32 @@ import Contact from './pages/Contact';
 import Resume from './pages/Resume';
 import Blog from './pages/Blog';
 import BlogPost from './pages/BlogPost';
+import AdminLogin from './pages/AdminLogin';
+import AdminDashboard from './components/AdminDashboard';
 import './index.css';
 
 function App() {
   return (
     <ThemeProvider>
-      <Router>
-        <div className="min-h-screen bg-gradient-to-br from-gray-900 to-black dark:from-gray-900 dark:to-black light:from-gray-50 light:to-white text-white dark:text-white light:text-gray-900 transition-colors duration-300">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/resume" element={<Resume />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/blog/:id" element={<BlogPost />} />
-          </Routes>
-          <ScrollToTop />
-        </div>
-      </Router>
+      <DataProvider>
+        <Router>
+          <div className="min-h-screen bg-gradient-to-br from-gray-900 to-black dark:from-gray-900 dark:to-black light:from-gray-50 light:to-white text-white dark:text-white light:text-gray-900 transition-colors duration-300">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/resume" element={<Resume />} />
+              <Route path="/blog" element={<Blog />} />
+              <Route path="/blog/:id" element={<BlogPost />} />
+              <Route path="/admin" element={<AdminLogin />} />
+              <Route path="/admin/dashboard" element={<AdminDashboard />} />
+            </Routes>
+            <ScrollToTop />
+          </div>
+        </Router>
+      </DataProvider>
     </ThemeProvider>
   );
 }
